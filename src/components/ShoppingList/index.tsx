0a08c4f57349a34e5ec60ef908af683f758ca28d
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-02-23 02:29:49 -0300

    get data from firestore

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,22 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FlatList } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 import { styles } from './styles';
 import { Product, ProductProps } from '../Product';
 
-import { shoppingListExample } from '../../utils/shopping.list.data';
-
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+	const [products, setProducts] = useState<ProductProps[]>([]);
 
-  return (
-    <FlatList
-      data={products}
-      keyExtractor={item => item.id}
-      renderItem={({ item }) => <Product data={item} />}
-      showsVerticalScrollIndicator={false}
-      style={styles.list}
-      contentContainerStyle={styles.content}
-    />
-  );
+	useEffect(() => {
+		firestore()
+			.collection('products')
+			.get()
+			.then((response) => {
+				const data = response.docs.map((doc) => {
+					return {
+						id: doc.id,
+						...doc.data(),
+					};
+				}) as ProductProps[];
+				setProducts(data);
+			})
+			.catch((error) => console.error(error));
+	}, []);
+
+	// useEffect(() => {
+	// 	firestore()
+	// 		.collection('products')
+	// 		.doc('fHka2JOFUuo32r3Rhmll')
+	// 		.get()
+	// 		.then((response) => console.log({ id: response.id, ...response.data() }));
+	// }, []);
+
+	return (
+		<FlatList
+			data={products}
+			keyExtractor={(item) => item.id}
+			renderItem={({ item }) => <Product data={item} />}
+			showsVerticalScrollIndicator={false}
+			style={styles.list}
+			contentContainerStyle={styles.content}
+		/>
+	);
 }
