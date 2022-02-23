1c83b324d111d3e60bd9e0018a287f5ecd866c63
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-02-23 02:09:28 -0300

    add doccument at collection

--- src/components/FormBox/index.tsx
+++ src/components/FormBox/index.tsx
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 import { Container } from './styles';
 import { ButtonIcon } from '../ButtonIcon';
 import { Input } from '../Input';
 
 export function FormBox() {
-  return (
-    <Container>
-      <Input
-        placeholder="Nome do produto"
-        size="medium"
-      />
+	const [description, setDescription] = useState('');
+	const [quantity, setQuantity] = useState(0);
 
-      <Input
-        placeholder="0"
-        keyboardType="numeric"
-        size="small"
-        style={{ marginHorizontal: 8 }}
-      />
+	async function handleProductAdd() {
+		firestore()
+			.collection('products')
+			.add({
+				description,
+				quantity,
+				done: false,
+        createdAt: firestore.FieldValue.serverTimestamp(),
+			})
+			.then(() => {
+				Alert.alert('Produto adicionado com sucesso');
+			})
+			.catch((error) => console.error(error));
+	}
 
-      <ButtonIcon
-        size='large'
-        icon="add-shopping-cart"
-        onPress={() => { }}
-      />
-    </Container>
-  );
+	return (
+		<Container>
+			<Input
+				placeholder='Nome do produto'
+				size='medium'
+				onChangeText={setDescription}
+			/>
+
+			<Input
+				placeholder='0'
+				keyboardType='numeric'
+				size='small'
+				style={{ marginHorizontal: 8 }}
+				onChangeText={(value) => setQuantity(Number(value))}
+			/>
+
+			<ButtonIcon
+				size='large'
+				icon='add-shopping-cart'
+				onPress={handleProductAdd}
+			/>
+		</Container>
+	);
 }
