df1f5d4bb5a9907aff0247e68f9520db36890c64
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-02-24 02:01:19 -0300

    create function to upload image

--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -1,51 +1,56 @@
 import React, { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
+import storage from '@react-native-firebase/storage';
 
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Photo } from '../../components/Photo';
 
 import { Container, Content, Progress, Transferred } from './styles';
+import { Alert } from 'react-native';
 
 export function Upload() {
-  const [image, setImage] = useState('');
+	const [image, setImage] = useState('');
 
-  async function handlePickImage() {
-    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+	async function handlePickImage() {
+		const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
-    if (status == 'granted') {
-      const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        aspect: [4, 4],
-        quality: 1,
-      });
+		if (status == 'granted') {
+			const result = await ImagePicker.launchImageLibraryAsync({
+				mediaTypes: ImagePicker.MediaTypeOptions.Images,
+				aspect: [4, 4],
+				quality: 1,
+			});
 
-      if (!result.cancelled) {
-        setImage(result.uri);
-      }
-    }
-  };
+			if (!result.cancelled) {
+				setImage(result.uri);
+			}
+		}
+	}
 
-  return (
-    <Container>
-      <Header title="Lista de compras" />
+	async function handleUpload() {
+		const fileName = new Date().getTime(); //estratégia para não ter nomes repetidos
+		const reference = storage().ref(`/images/${fileName}.png`);
 
-      <Content>
-        <Photo uri={image} onPress={handlePickImage} />
+		reference
+			.putFile(image)
+			.then(() => Alert.alert('Sucesso', 'Imagem enviada com sucesso'))
+			.catch((error) => console.error(error));
+	}
 
-        <Button
-          title="Fazer upload"
-          onPress={() => { }}
-        />
+	return (
+		<Container>
+			<Header title='Lista de compras' />
 
-        <Progress>
-          0%
-        </Progress>
+			<Content>
+				<Photo uri={image} onPress={handlePickImage} />
 
-        <Transferred>
-          0 de 100 bytes transferido
-        </Transferred>
-      </Content>
-    </Container>
-  );
+				<Button title='Fazer upload' onPress={handleUpload} />
+
+				<Progress>0%</Progress>
+
+				<Transferred>0 de 100 bytes transferido</Transferred>
+			</Content>
+		</Container>
+	);
 }
