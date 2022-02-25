21155c505d24c1a7b87a657e6df04c0d622f959d
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-02-24 23:22:25 -0300

    logout function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ButtonIcon } from '../ButtonIcon';
+import auth from '@react-native-firebase/auth';
 
 import { Container, Title } from './styles';
 
@@ -9,6 +10,10 @@ type Props = {
 }
 
 export function Header({ title, showLogoutButton = false }: Props) {
+  function handleLogout() {
+    auth().signOut();
+  }
+
   return (
     <Container showLogoutButton={showLogoutButton}>
       <Title>
@@ -21,7 +26,7 @@ export function Header({ title, showLogoutButton = false }: Props) {
           icon="logout"
           color="alert"
           style={{ marginTop: 20 }}
-          onPress={() => {}}
+          onPress={handleLogout}
         />
       }
     </Container>
