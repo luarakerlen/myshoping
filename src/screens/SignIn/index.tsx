0b7dfb2994210307343940837d4c8a76b99a26ae
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-02-24 23:13:24 -0300

    create create-user and login functions

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,32 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
+import auth from '@react-native-firebase/auth';
 
 import { Container, Account, Title, Subtitle } from './styles';
 import { ButtonText } from '../../components/ButtonText';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
+import { Alert } from 'react-native';
+import theme from '../../theme';
 
 export function SignIn() {
-  return (
-    <Container>
-      <Title>MyShopping</Title>
-      <Subtitle>monte sua lista de compra te ajudar nas compras</Subtitle>
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-      <Input
-        placeholder="e-mail"
-        keyboardType="email-address"
-      />
+	async function handleSignInAnonymously() {
+		const { user } = await auth().signInAnonymously();
+		console.log(user);
+	}
 
-      <Input
-        placeholder="senha"
-        secureTextEntry
-      />
+	function handleCreateUserAccount() {
+		auth()
+			.createUserWithEmailAndPassword(email, password)
+			.then(() => Alert.alert('Usuário criado com sucesso!'))
+			.catch((error) => {
+				switch (error.code) {
+					case 'auth/email-already-in-use':
+						Alert.alert('E-mail já está em uso!');
+						break;
+					case 'auth/invalid-email':
+						Alert.alert('E-mail inválido!');
+						break;
+					case 'auth/weak-password':
+						Alert.alert('A senha deve ter no mínimo 6 caracteres!');
+						break;
 
-      <Button title="Entrar" onPress={() => { }} />
+					default:
+						Alert.alert('Erro ao criar usuário!');
+						break;
+				}
+			});
+	}
 
-      <Account>
-        <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
-      </Account>
-    </Container>
-  );
+  function handleSignInWithEmailAndPassword() {
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        switch (error.code) {
+          case 'auth/invalid-email':
+            Alert.alert('E-mail inválido!');
+            break;
+          case 'auth/user-not-found':
+            Alert.alert('Usuário não encontrado!');
+            break;
+          case 'auth/wrong-password':
+            Alert.alert('Senha incorreta!');
+            break;
+
+          default:
+            Alert.alert('Erro ao logar!');
+            break;
+        }
+      });
+  }
+
+	return (
+		<Container>
+			<Title>MyShopping</Title>
+			<Subtitle>monte sua lista de compra te ajudar nas compras</Subtitle>
+
+			<Input
+				placeholder='e-mail'
+				keyboardType='email-address'
+				onChangeText={setEmail}
+			/>
+
+			<Input placeholder='senha' secureTextEntry onChangeText={setPassword} />
+
+			<Button title='Entrar' onPress={handleSignInWithEmailAndPassword} />
+			<Button
+				title='Entrar sem login'
+				onPress={handleSignInAnonymously}
+				style={{
+					backgroundColor: theme.COLORS.GRAY800,
+					marginTop: 10,
+				}}
+			/>
+
+			<Account>
+				<ButtonText title='Recuperar senha' onPress={() => {}} />
+				<ButtonText
+					title='Criar minha conta'
+					onPress={handleCreateUserAccount}
+				/>
+			</Account>
+		</Container>
+	);
 }
