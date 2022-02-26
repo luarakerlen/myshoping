d80703e8fc113f64b8432cadf68de548c4493184
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-02-26 03:48:16 -0300

    configure category dropdown

--- src/components/FormBox/index.tsx
+++ src/components/FormBox/index.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 import firestore from '@react-native-firebase/firestore';
 
-import { Container } from './styles';
+import { Container, AllContent, styles } from './styles';
 import { ButtonIcon } from '../ButtonIcon';
 import { Input } from '../Input';
+import SelectDropdown from 'react-native-select-dropdown';
+import { categoriesColors } from '../../enums';
+import FontAwesome from '@expo/vector-icons/build/FontAwesome';
 
 export function FormBox() {
 	const [description, setDescription] = useState('');
 	const [quantity, setQuantity] = useState(0);
+	const [category, setCategory] = useState<
+		Array<keyof typeof categoriesColors>
+	>(['outros']);
+
+	const categories: string[] = Object.entries(categoriesColors)
+		.map(([key]) => key)
+		.sort();
 
 	async function handleProductAdd() {
 		firestore()
@@ -17,37 +27,70 @@ export function FormBox() {
 				quantity,
 				done: false,
 				createdAt: firestore.FieldValue.serverTimestamp(),
+				categories: category,
 			})
 			.then(() => {
 				setDescription('');
 				setQuantity(0);
+				setCategory(['outros']);
 			})
 			.catch((error) => console.error(error));
 	}
 
 	return (
-		<Container>
-			<Input
-				placeholder='Nome do produto'
-				size='medium'
-				onChangeText={setDescription}
-				value={description}
-			/>
+		<AllContent>
+			<Container>
+				<Input
+					placeholder='Nome do produto'
+					size='medium'
+					onChangeText={setDescription}
+					value={description}
+				/>
 
-			<Input
-				placeholder='0'
-				keyboardType='numeric'
-				size='small'
-				style={{ marginHorizontal: 8 }}
-				onChangeText={(value) => setQuantity(Number(value))}
-				value={quantity ? String(quantity) : ''}
-			/>
+				<Input
+					placeholder='0'
+					keyboardType='numeric'
+					size='small'
+					style={{ marginHorizontal: 8 }}
+					onChangeText={(value) => setQuantity(Number(value))}
+					value={quantity ? String(quantity) : ''}
+				/>
 
-			<ButtonIcon
-				size='large'
-				icon='add-shopping-cart'
-				onPress={handleProductAdd}
+				<ButtonIcon
+					size='large'
+					icon='add-shopping-cart'
+					onPress={handleProductAdd}
+				/>
+			</Container>
+			<SelectDropdown
+				data={categories}
+				onSelect={(value) => setCategory([value])}
+				buttonTextAfterSelection={(selectedItem) => {
+					return selectedItem;
+				}}
+				rowTextForSelection={(item) => {
+					return item;
+				}}
+				defaultButtonText='Selecione a categoria'
+				dropdownIconPosition={'right'}
+				renderDropdownIcon={(isOpened) => {
+					return (
+						<FontAwesome
+							name={isOpened ? 'chevron-up' : 'chevron-down'}
+							color={categoriesColors[category[0]]}
+							size={18}
+						/>
+					);
+				}}
+				dropdownStyle={styles.dropdown1DropdownStyle}
+				rowStyle={styles.dropdown1RowStyle}
+				rowTextStyle={styles.dropdown1RowTxtStyle}
+				buttonStyle={[
+					styles.dropdown1BtnStyle,
+					{ borderColor: categoriesColors[category[0]] },
+				]}
+				buttonTextStyle={styles.dropdown1BtnTxtStyle}
 			/>
-		</Container>
+		</AllContent>
 	);
 }
--- src/components/FormBox/styles.ts
+++ src/components/FormBox/styles.ts
@@ -1,11 +1,34 @@
 import styled from 'styled-components/native';
+import { StyleSheet } from 'react-native';
 
-export const Container = styled.View`
-  width: 100%;
-  flex-direction: row;
-  padding-left: 24px;
-  padding-right: 24px;
-  margin-top: 24px;
-  margin-bottom: 24px;
+export const AllContent = styled.View`
+	padding-left: 24px;
+	padding-right: 24px;
+	margin-top: 24px;
+	margin-bottom: 4px;
 `;
 
+export const Container = styled.View`
+	width: 100%;
+	flex-direction: row;
+`;
+
+export const styles = StyleSheet.create({
+	dropdown1DropdownStyle: { backgroundColor: '#EFEFEF' },
+
+	dropdown1BtnStyle: {
+		backgroundColor: '#FFF',
+		borderRadius: 8,
+		borderWidth: 2,
+		width: '100%',
+	},
+
+	dropdown1BtnTxtStyle: { color: '#444', textAlign: 'center' },
+
+	dropdown1RowStyle: {
+		backgroundColor: '#EFEFEF',
+		borderBottomColor: '#C5C5C5',
+	},
+
+	dropdown1RowTxtStyle: { color: '#444', textAlign: 'left' },
+});
